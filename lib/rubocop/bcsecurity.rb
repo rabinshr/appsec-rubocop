33d43410d02a8f598c15d4d97a277cce0190fadf
Evan Johnson <evan.johnson@bigcommerce.com>
2023-02-10 12:45:07 -0800

    use frozen_string_literal

--- lib/rubocop/bcsecurity.rb
+++ lib/rubocop/bcsecurity.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 module RuboCop
   module BcSecurity
-    PROJECT_ROOT   = Pathname.new(__dir__).parent.parent.expand_path.freeze
-    CONFIG_DEFAULT = PROJECT_ROOT.join('config', 'default.yml').freeze
-    CONFIG = YAML.safe_load(CONFIG_DEFAULT.read).freeze
+    PROJECT_ROOT   = Pathname.new(__dir__).parent.parent.expand_path
+    CONFIG_DEFAULT = PROJECT_ROOT.join('config', 'default.yml')
+    CONFIG = YAML.safe_load(CONFIG_DEFAULT.read)
 
     private_constant(:CONFIG_DEFAULT, :PROJECT_ROOT)
   end
