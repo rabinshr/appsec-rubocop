51f4e812f291678586c92205dab2821c1bbe1143
Evan Johnson <evan.johnson@bigcommerce.com>
2023-02-13 11:59:26 -0800

    add detection for `message=...;log(message)` case

--- lib/rubocop/cop/bcsecurity/logging.rb
+++ lib/rubocop/cop/bcsecurity/logging.rb
@@ -7,10 +7,19 @@ module RuboCop
       # Reason: We recently removed nearly 50 instances of this from BigPay.
       class LoggingRawPost < RuboCop::Cop::Base
         def_node_matcher :logs_raw_post, <<~PATTERN
-          (send
+          {(send
             (send
-              (const nil? :Rails) :logger) $_
-            <`({send | ivar} ... {:raw_post | :@raw_post}) ...>)
+              (const nil? :Rails) :logger) _
+            <`({send | ivar} ... {:raw_post | :@raw_post}) ...>) | (send nil? :log_from <`{:raw_post | :@raw_post} ...>)}
+        PATTERN
+
+        def_node_matcher :dangerous_string_assign, <<~PATTERN
+          (lvasgn $_
+              <`{:raw_post | :@raw_post} ...>)
+        PATTERN
+
+        def_node_matcher :logs_name, <<~PATTERN
+          {(send (send (const nil? :Rails) :logger) _ <`%name ...>) | (send const nil? :log_from <`%name ...>)}
         PATTERN
 
         MSG = 'You appear to be logging raw POST data, this could result in logging secret values.'
@@ -18,6 +27,32 @@ module RuboCop
         def on_send(node)
           logs_raw_post(node) do |_|
             add_offense(node)
+          end
+        end
+
+        def on_begin(node)
+          dangerous_names = {'raw_post'=>nil, '@raw_post'=>nil}
+          node.each_child_node do |child|
+            if child.lvasgn_type?
+              dangerous_string_assign(child) do |name|
+                dangerous_names[name.to_sym] = child
+              end
+            else
+              mark_offenses(child, dangerous_names)
+            end
+          end
+        end
+
+        def mark_offenses(node, names)
+          names.each do |name, source_node|
+            if logs_name(node, :name=>name)
+              if !source_node
+                add_offense(node)
+              else
+                add_offense(source_node)
+                add_offense(node, message: format("`%<name>s` passed to logging call here.", name: name))
+              end
+            end
           end
         end
       end
--- spec/lib/rubocop/cop/bcsecurity/logging_spec.rb
+++ spec/lib/rubocop/cop/bcsecurity/logging_spec.rb
@@ -50,5 +50,66 @@ RSpec.describe RuboCop::Cop::BcSecurity::LoggingRawPost, :config do
         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ #{RuboCop::Cop::BcSecurity::LoggingRawPost::MSG}
       RUBY
     end
+
+    it 'detects messages constructed prior to logging' do
+      expect_offense(<<~RUBY)
+        def foo
+          message = "some message: \#{raw_post}"
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ #{RuboCop::Cop::BcSecurity::LoggingRawPost::MSG}
+          Rails.logger.info(message, other)
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ `message` passed to logging call here.
+        end
+      RUBY
+    end
+
+    it 'detects real example using `log_from' do
+      expect_offense(<<~RUBY)
+        log_from(
+        ^^^^^^^^^ #{RuboCop::Cop::BcSecurity::LoggingRawPost::MSG}
+          method: __method__,
+          status: :error,
+          msg: "Receive notification with invalid gateway or profile: raw post=\#{raw_post}",
+          data: { store_id: store_id, gateway: gateway }
+        )
+      RUBY
+    end
+
+    it 'detects real example constructing message and passing it to a logging call' do
+      expect_offense(<<~RUBY)
+        def perform(notification_request)
+          store_id, gateway, raw_post = notification_request.values_at(:store_id, :gateway, :raw_post)
+          raise(BigCommerce::Error::Invalid, 'Invalid gateway') if gateway.blank?
+          raise(BigCommerce::Error::Invalid, 'Invalid raw_post') if raw_post.blank?
+
+          message = "[\#{gateway}_webhooks] Starting notification process for store: \#{store_id}, (raw_post: \#{raw_post})"
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ #{RuboCop::Cop::BcSecurity::LoggingRawPost::MSG}
+          Rails.logger.info(message, gateway: gateway, store_id: store_id)
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ `message` passed to logging call here.
+          BigPay::Payments::Notification::Processor::StripeUpe::StripeUpeNotificationProcessor.new(store_id, gateway, nil, raw_post).process_notification
+          message = "[\#{gateway}_webhooks] Finish notification process for store: \#{store_id}, (raw_post: \#{raw_post})"
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ #{RuboCop::Cop::BcSecurity::LoggingRawPost::MSG}
+          Rails.logger.info(message, gateway: gateway, store_id: store_id)
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ `message` passed to logging call here.
+        rescue => e
+          Rails.logger.error("[\#{gateway}_webhooks] Error while processing notification for store: \#{store_id}", gateway: gateway, store_id: store_id, message: e.message)
+          raise e
+        end
+      RUBY
+    end
+
+    it 'permits dynamic string construction for signature checks' do
+      expect_no_offenses(<<~RUBY)
+        def parse_authentication_header(x_affirm_signature_multi)
+          timestamp, hashed_payload = x_affirm_signature_multi.split(',')
+          hashed_payload_keys = hashed_payload.gsub(/v0=/, '').split('=')
+          timestamp = timestamp.gsub(/t=/, '')
+          return nil unless validate_timestamp(timestamp.to_i)
+
+          payload = "\#{timestamp}.\#{@request.raw_post}"
+
+          [payload, hashed_payload_keys]
+        end
+      RUBY
+    end
   end
 end
